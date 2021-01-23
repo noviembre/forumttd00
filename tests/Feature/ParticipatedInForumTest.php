bf4bbcbdff8c623634ad89f97da22f7fc8edfc35
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-23 15:20:25 -0500

    Missin Complete - Lesson 4 - Tested OK
    finalmente, despues de varios intentos lo consegui...pase este nivel.
    apesar que ya lo habia hecho.

--- tests/Feature/ParticipatedInForumTest.php
+++ tests/Feature/ParticipatedInForumTest.php
@@ -10,13 +10,21 @@ class ParticipatedInForumTest extends TestCase
 
     use DatabaseMigrations;
 
+    /** @test */
+    public function an_authenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->post('/threads/1/replies', []);
+
+    }
+
     /**  @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         $this->be($user = factory('App\User')->create());
         $thread = factory('App\Thread')->create();
 
-        $reply = factory('App\Reply')->create();
+        $reply = factory('App\Reply')->make();
 
         $this->post($thread->path() . '/replies', $reply->toArray());
 
