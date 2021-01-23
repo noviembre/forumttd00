832f387a3717a22670c71f58a2e01794004a5b49
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-23 11:39:27 -0500

    2.3 Listing all threads
    en threads/index. se hizo tan solo eso.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/threads','ThreadsController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -2,10 +2,20 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ThreadTest extends TestCase
 {
-    
+
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+        $response->assertStatus(200);
+
+    }
 }
