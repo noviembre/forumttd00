cb25cb3e4c79e856e8ea325f8a47dd8f6f0c489a
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-23 14:34:52 -0500

    2. it was tested Ok

--- tests/Unit/ThreadsTest.php
+++ tests/Unit/ThreadsTest.php
@@ -7,11 +7,20 @@ use Tests\TestCase;
 
 class ThreadsTest extends TestCase
 {
+
     use DatabaseMigrations;
+
     /** @test */
     public function a_thread_has_replies()
     {
         $thread = factory('App\Thread')->create();
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_has_a_creator()
+    {
+        $thread = factory('App\Thread')->create();
+        $this->assertInstanceOf('App\User', $thread->creator);
     }
 }
