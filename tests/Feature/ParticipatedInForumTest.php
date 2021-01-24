b0770c1d62ffbd6f78d5ce1d0b30018a47adc6d3
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-24 05:30:25 -0500

    2. Using Helpers

--- tests/Feature/ParticipatedInForumTest.php
+++ tests/Feature/ParticipatedInForumTest.php
@@ -21,10 +21,10 @@ class ParticipatedInForumTest extends TestCase
     /**  @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->be($user = factory('App\User')->create());
-        $thread = factory('App\Thread')->create();
+        $this->be($user = create('App\User'));
+        $thread = create('App\Thread');
 
-        $reply = factory('App\Reply')->make();
+        $reply = make('App\Reply');
 
         $this->post($thread->path() . '/replies', $reply->toArray());
 
