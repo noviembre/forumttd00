4a93eac9f152e4bfafe019ceaceaf8b2e973370e
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-24 05:50:28 -0500

    1. Some Routes are sleeping

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/threads','ThreadsController@index');
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/threads', 'ThreadsController@store');
-Route::get('/threads/{thread}','ThreadsController@show');
+
+//Route::get('/threads','ThreadsController@index');
+//Route::get('/threads/create','ThreadsController@create');
+//Route::post('/threads', 'ThreadsController@store');
+//Route::get('/threads/{thread}','ThreadsController@show');
+Route::resource('threads','ThreadsController');
 
 Route::post('/threads/{thread}/replies','RepliesController@store');
 
