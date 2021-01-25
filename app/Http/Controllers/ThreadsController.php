7cf6741e0a5da9d0d1f02a678297eaa260836c50
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-25 06:29:04 -0500

    5.2 validate channel attribute - Tested Ok

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -47,7 +47,7 @@ class ThreadsController extends Controller
         $this->validate($request, [
             'title' => 'required',
             'body' => 'required',
-            'channel_id' => 'required',
+            'channel_id' => 'required|exists:channels,id',
         ]);
 
         $thread = Thread::create([
