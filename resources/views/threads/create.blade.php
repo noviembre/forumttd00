47b0f2bc20d449f2a86261ef3de945b98a52160a
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-26 05:01:52 -0500

    2. everytime a category is selected, will not be forget
    there is a condition where when you select a category and fill the form
    the category selected before will stay there.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -14,21 +14,23 @@
                             @csrf
                             <div class="form-group">
                                 <label for="channel_id">Choose a Channel:</label>
-                                <select name="channel_id" id="channel_id" class="form-control">
+                                <select name="channel_id" id="channel_id" class="form-control" required>
                                     <option value="">Choose One...</option>
                                     @foreach(App\Channel::all() as $channel)
-                                        <option value="{{$channel->id}}">{{$channel->name}}</option>
+                                        <option value="{{$channel->id}}" {{old('channel_id') == $channel->id ? 'selected': ''}}>
+                                            {{$channel->name}}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input name="title" value="{{old('title')}}" type="text" class="form-control" id="title">
+                                <input name="title" value="{{old('title')}}" type="text" class="form-control" id="title" required>
                             </div>
 
                             <div class="form-group">
                                 <label for="body">Body:</label>
-                                <textarea name="body" id="body" class="form-control" rows="8">{{old('body')}}</textarea>
+                                <textarea name="body" id="body" class="form-control" rows="8" required>{{old('body')}}</textarea>
                             </div>
 
                             <div class="form-group">
