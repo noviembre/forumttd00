5d5d8565a2c28bfb8e9d743b88bcccc1dde0f4c3
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-26 06:12:33 -0500

    a user can list hiw own threads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,23 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/threads">All Threads</a>
                         </li>
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Browse <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/threads/">
+                                        All Threads
+                                    </a>
+                                <a class="dropdown-item" href="/threads?by={{auth()->user()->name}}">
+                                        My Threads
+                                </a>
+
+                            </div>
+                        </li>
+
                         <li class="nav-item">
                             <a class="nav-link" href="/threads/create">New Thread</a>
                         </li>
