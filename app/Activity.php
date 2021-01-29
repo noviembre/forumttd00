d778f9eb97af6ddad00a0e8f15797ed5fb80645d
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-29 06:56:19 -0500

    Adding more things

--- app/Activity.php
+++ app/Activity.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
+
     protected $guarded = [];
+
+    public function subject()
+    {
+        return $this->morphTo();
+    }
 
 }
