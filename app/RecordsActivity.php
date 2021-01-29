9126904c09e1da764e3028910f5d86c414cc8159
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-29 06:50:35 -0500

    more Refactoring ..

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -16,13 +16,17 @@ trait RecordsActivity
 
     public function recordActivity($event)
     {
-        Activity::create([
-            'user_id'      => auth()->id(),
-            'type'         => $this->getActivityType($event),
-            'subject_id'   => $this->id,
-            'subject_type' => get_class($this),
+        $this->activity()->create([
+            'user_id' => auth()->id(),
+            'type'    => $this->getActivityType($event),
         ]);
     }
+
+    public function activity()
+    {
+        return $this->morphMany('App\Activity','subject');
+    }
+
 
     protected function getActivityType($event)
     {
