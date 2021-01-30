a5180b2bf340d921033766b9d96f906580174542
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-30 12:03:30 -0500

    Edit Reply, deleting a blinking frame
    everytime you press F5, in the texte area where is the content of reply, there was a blick frame,
    but not anymore.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
         body {padding-bottom: 100px;}
         .level {display: flex;align-items: center;}
         .flex {flex:1;}
+        [v-clock]{ display: none;}
     </style>
 </head>
 <body>
