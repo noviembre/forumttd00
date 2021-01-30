617950734c45d3537ea5546e89368c11eb137912
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-30 11:16:00 -0500

    working on edit textarea

---
+++ resources/js/components/Reply.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        data(){
+            return {
+                editing:false
+            };
+        }
+
+    }
+</script>
+
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,39 +1,49 @@
-<div id="reply-{{ $reply->id }}" class="card">
+<reply inline-template>
+    <div id="reply-{{ $reply->id }}" class="card">
 
-    <div class="card-header">
-        <div class="level">
-            <h5 class="flex">
-                <a href="{{ route('profile', $reply->owner) }}">
-                    {{ $reply->owner->name }}
-                </a>
-                said {{ $reply->created_at->diffForHumans() }}
-            </h5>
+        <div class="card-header">
+            <div class="level">
+                <h5 class="flex">
+                    <a href="{{ route('profile', $reply->owner) }}">
+                        {{ $reply->owner->name }}
+                    </a>
+                    said {{ $reply->created_at->diffForHumans() }}
+                </h5>
 
-            <div>
-            <form method="post" action="/replies/{{$reply->id}}/favorites">
-                @csrf
-                <button type="submit" class="btn btn-secondary btn-sm" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                    {{ $reply->favorites_count }}
-                    {{ str_plural('Favorite', $reply->favorites_count)  }}
-                </button>
-            </form>
+                <div>
+                    <form method="post" action="/replies/{{$reply->id}}/favorites">
+                        @csrf
+                        <button type="submit" class="btn btn-secondary btn-sm" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                            {{ $reply->favorites_count }}
+                            {{ str_plural('Favorite', $reply->favorites_count)  }}
+                        </button>
+                    </form>
+
+                </div>
+            </div>
 
         </div>
+        <div class="card-body">
+            <div v-if="editing">
+                <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+
+            </div>
+
+            <div v-else>
+                {{ $reply->body }}
+            </div>
         </div>
 
+        @can('update',$reply)
+            <div class="card-footer level">
+                <button class="btn btn-info btn-sm mr-1" @click="editing = true">Edit</button>
+                <form action="/replies/{{$reply->id}}" method="post">
+                    @csrf
+                    {{ method_field('DELETE') }}
+                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                </form>
+
+            </div>
+        @endcan
     </div>
-    <div class="card-body">
-        {{ $reply->body }}
-    </div>
-    @can('update',$reply)
-        <div class="card-footer level">
-            <button class="btn btn-info btn-sm mr-1">Edit</button>
-            <form action="/replies/{{$reply->id}}" method="post">
-                @csrf
-                {{ method_field('DELETE') }}
-                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-            </form>
-
-        </div>
-    @endcan
-</div>
+</reply>
