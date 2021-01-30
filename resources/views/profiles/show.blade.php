163f8f3ab35b26670825b788b3e86460b16b7ae6
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-01-30 06:42:23 -0500

    if a profile activity exits... show it

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -12,8 +12,11 @@
 
                 @foreach($activities as $date => $activity)
                     <h3>{{ $date }}</h3>
+
                     @foreach($activity as $record)
-                    @include("profiles.activities.{$record->type}", ['activity' => $record])
+                        @if(view()->exists("profiles.activities.{$record->type}"))
+                            @include("profiles.activities.{$record->type}", ['activity' => $record])
+                        @endif
                     @endforeach
                 @endforeach
 
