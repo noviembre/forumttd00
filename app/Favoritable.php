01ae1806c5e292465a8d8d7af2db96e0f7cdfc59
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-01 11:46:52 -0500

    fixing a bug

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -19,7 +19,7 @@ trait Favoritable
     {
         $attributes = [ 'user_id' => auth()->id() ];
 
-        $this->favorites()->where($attributes)->delete();
+        $this->favorites()->where($attributes)->get()->each->delete();
 
     }
 
