3ab422720e0336061aff0a8244106729010dd974
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-08 14:53:19 -0500

    The bug was Fixed.
    the issue about notification was fixed and is workind.

--- resources/js/components/UserNotifications.vue
+++ resources/js/components/UserNotifications.vue
@@ -1,9 +1,42 @@
 <template>
-    <div>Hello world</div>
+    <li class="nav-item dropdown" v-if="notifications.length">
+
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+             <span class="fa fa-bell caret"></span>
+        </a>
+
+        <div v-for="notification in notifications" class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item"
+               :href="notification.data.link"
+               v-text="notification.data.message"
+               @click="markAsRead(notification)"
+
+            >
+                Foobar
+            </a>
+
+        </div>
+    </li>
 </template>
 
 <script>
     export default{
+
+        data(){
+            return { notifications:false}
+        },
+
+        created(){
+            axios.get("/profiles/" + window.App.user.name + "/notifications")
+                .then(response => this.notifications = response.data);
+
+        },
+
+        methods: {
+            markAsRead(notification) {
+                axios.delete('/profiles/' + window.App.user.name + '/notifications/' + notification.id)
+            }
+        }
 
     }
 </script>
