df968d9ae0822cad519281b645be1781d32dcaca
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-09 04:54:32 -0500

    cleaning the file

--- app/Events/ThreadHasNewReply.php
+++ app/Events/ThreadHasNewReply.php
@@ -1,18 +1,11 @@
 <?php
 
 namespace App\Events;
-
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class ThreadHasNewReply
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
@@ -22,15 +15,5 @@ class ThreadHasNewReply
     public function __construct()
     {
         //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
