b12ae0bcf9cde8958e5bfb1f5788aa345a8d6eb5
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-19 08:09:40 -0500

    fixing some test problems
    which occur due to the new code. (I suppose so)

--- tests/Feature/ReadThreadTest.php
+++ tests/Feature/ReadThreadTest.php
@@ -67,7 +67,7 @@ class ReadThreadTest extends TestCase
 
         $threadsWith_0Replies = $this->thread;
         $response = $this->getJson('threads?popular=1')->json();
-        $this->assertEquals([ 3, 2, 0 ], array_column($response, 'replies_count'));
+        $this->assertEquals([ 3, 2, 0 ], array_column($response['data'], 'replies_count'));
     }
 
 
@@ -79,7 +79,7 @@ class ReadThreadTest extends TestCase
 
         $response = $this->getJson('threads?unanswered=1')->json();
 
-        $this->assertCount(1, $response);
+        $this->assertCount(1, $response['data']);
     }
 
 
