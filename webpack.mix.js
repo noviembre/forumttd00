c2a19a13e9fc51f334076c3cab64822774d4339a
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-19 07:40:38 -0500

    some code
    I forgot to some semicolon and another thing in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-const mix = require('laravel-mix');
-
+// const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
