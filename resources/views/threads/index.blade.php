588ef67bb8bf77d6948b3b53a269b397c98099ff
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-19 08:06:05 -0500

    Just Refactoring and paginate

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,48 +4,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                @forelse($threads as $thread)
-                <div class="card">
-                    <div class="card-header">
-                        <div class="level">
-                            <div class="flex">
-                                <h4>
-                                    <a href="{{ $thread->path() }}">
+                @include('threads._list')
 
-                                        @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                            <strong>
-                                                {{ $thread->title }}
-                                            </strong>
-
-                                        @else
-                                            {{ $thread->title }}
-                                        @endif
-                                    </a>
-                                </h4>
-
-                                <h5>
-                                    Posted By:
-                                    <a href="{{ route('profile', $thread->creator) }}">
-                                        {{ $thread->creator->name }}
-                                    </a>
-
-                                </h5>
-                            </div>
-
-                            <a href="{{ $thread->path() }}">
-                                {{$thread->replies_count}}
-                                {{ str_plural('reply',$thread->replies_count) }}
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        {{ $thread->body }}
-                    </div>
-                </div>
-                    @empty
-                    <p>Therere not relevant results at time</p>
-                @endforelse
+                {{ $threads->render() }}
             </div>
         </div>
     </div>
