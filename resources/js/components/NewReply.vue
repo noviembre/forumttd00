869ce68f5fd168ad2568a5bc20d53644ab237bf6
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-02-19 05:41:34 -0500

    Some code

--- resources/js/components/NewReply.vue
+++ resources/js/components/NewReply.vue
@@ -30,6 +30,9 @@
 </template>
 
 <script>
+
+    import 'at.js';
+
     export default{
         data(){
             return {
@@ -42,6 +45,21 @@
             signedIn(){
                 return window.App.signedIn;
             }
+        },
+
+        mounted(){
+            $('#body').atwho({
+                at: "@",
+                delay: 750,
+                callbacks: {
+                    remoteFilter: function (query, callback) {
+                        console.log('called');
+//                        $.getJSON("/user.php", {q: query}, function (usernames) {
+//                            callback(usernames)
+//                        });
+                    }
+                }
+            })
         },
 
         methods: {
